a99617457a668d39827b4fa74ffb7b4375463478
Luq01 <lucasgabrielsilvadeoliveira01@gmail.com>
2026-01-13 17:51:02 -0300

    Adição de paginas

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#mobile_barras').click(function() {
         $('#mobile_menu').toggleClass('active');
-    }); 
+        $('#mobile_barras').toggleClass('active');
+    });
 });
